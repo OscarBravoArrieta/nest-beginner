298ae36b249d4f7484a88fdb3290b1bc43a438c2
obravo <obravo@asercol.com>
2023-08-26 11:04:30 -0500

    obtaining user information /me

--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -2,12 +2,17 @@
  import { ExtractJwt, Strategy } from "passport-jwt"
  import { ConfigService } from "@nestjs/config"
  import { Injectable } from "@nestjs/common"
+ import { PrismaService } from "src/prisma/prisma.service"
+
 
  @Injectable({})
- export class JwtStrategy extends PassportStrategy (Strategy,
+ export class JwtStrategy extends PassportStrategy (
+     Strategy,
+     'jwt'
      ) {
      constructor(
-         config: ConfigService
+         config: ConfigService,
+         private prisma: PrismaService
      ) {
          super({
              jwtFromRequest:
@@ -15,4 +20,18 @@
                  secretOrKey: config.get('JWT_SECRET')
          })
      }
+     async validate(payload: {
+         sub: number;
+         email: string;
+      }) {
+         const user =
+             await this.prisma.user.findUnique({
+                 where: {
+                     id: payload.sub
+                 }
+             })
+         delete user.hash
+         return null
+     }
  }
+
